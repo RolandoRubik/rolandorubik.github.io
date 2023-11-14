44cabb0efebbff50f5d3f142c3ef852b6c62e91c
RolandoRubik <87745923+RolandoRubik@users.noreply.github.com>
2023-11-13 20:48:08 -0600

    Add files via upload

---
+++ funcion.js
@@ -0,0 +1,35 @@
+function isMobile() {
+    if (sessionStorage.desktop)
+        return false;
+    else if (localStorage.mobile)
+        return true;
+    var mobile = ['iphone', 'ipad', 'android', 'blackberry', 'nokia', 'opera mini', 'windows mobile', 'windows phone', 'iemobile'];
+    for (var i in mobile)
+        if (navigator.userAgent.toLowerCase().indexOf(mobile[i].toLowerCase()) > 0) return true;
+    return false;
+}
+
+const formulario = document.querySelector('#formulario');
+const buttonSubmit = document.querySelector('#submit');
+const urlDesktop = 'https://web.whatsapp.com/';
+const urlMobile = 'whatsapp://';
+const telefono = '527821462008';
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault()
+    buttonSubmit.innerHTML = '<i class="fas fa-circle-notch fa-spin"></i>'
+    buttonSubmit.disabled = true
+    setTimeout(() => {
+        let nombre = document.querySelector('#nombre').value
+        let adultos = document.querySelector('#adultos').value
+        let niños = document.querySelector('#niños').value
+        let mensaje = 'send?phone=' + telefono + '&text=*_Mensaje de confirmación para los XV Años de Karla Guadalupe_*%0A*Nombre:* ' + nombre + '%0A*Número de adultos:* ' + adultos + '%0A*Número de niños:* ' + niños + ''
+        if(isMobile()) {
+            window.open(urlMobile + mensaje, '_blank')
+        }else{
+            window.open(urlDesktop + mensaje, '_blank')
+        }
+        buttonSubmit.innerHTML = '<i class="fab fa-whatsapp"></i> Enviar WhatsApp'
+        buttonSubmit.disabled = false
+    }, 3000);
+});
